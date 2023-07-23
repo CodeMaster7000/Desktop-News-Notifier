037231894ab689850ed8cf0873726559e16552ea
CodeMaster7000 <CodeMaster7K@gmail.com>
2023-07-23 13:14:45 +0100

    Update NewsNotifier.py

---
+++ NewsNotifier.py
@@ -0,0 +1,19 @@
+import feedparser
+import notify2
+import os
+import time
+def parseFeed():
+    f = feedparser.parse("http://feeds.bbci.co.uk/news/rss.xml")
+    ICON_PATH = os.getcwd() + "/newsicon.ico"
+    notify2.init('News Notify')
+    for newsitem in f['items']: 
+        n = notify2.Notification(newsitem['title'], 
+                                 newsitem['summary'], 
+                                 icon=ICON_PATH 
+                                 )
+    n.set_urgency(notify2.URGENCY_NORMAL)
+    n.show()
+    n.set_timeout(15000)
+    time.sleep(1200)
+if __name__ == '__main__':
+    parseFeed()
